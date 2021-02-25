348057dd38b85eb0ba2021704a0eb5920f3118bb
Gpopcorn <75551887+Gpopcorn@users.noreply.github.com>
2021-02-25 16:25:35 -0700

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,14 @@
+def multiplyMatrix(a, b):
+    rowsA = len(a)
+    colsA = len(a[0])
+    rowsB = len(b)
+    colsB = len(b[0])
+
+    c = [[0 for row in range(colsB)] for col in range(rowsA)]
+
+    for x in range(rowsA):
+        for y in range(colsB):
+            for z in range(colsA):
+                c[x][y] += a[x][z] * b[z][y]
+
+    return c
